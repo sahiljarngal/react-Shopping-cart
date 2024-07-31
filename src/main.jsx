87410610b0dd4259ca07b1d9d3b6373aa60eb317
sahiljarngal <97387822+sahiljarngal@users.noreply.github.com>
2024-08-01 00:50:48 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import ContextProvider from "./Features/ContextProvider.jsx";
-import { HashRouter } from "react-router-dom";
+
 // use context api and pass App as a children to access all page
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </HashRouter>
+  <ContextProvider>
+    <App />
+  </ContextProvider>
 );
